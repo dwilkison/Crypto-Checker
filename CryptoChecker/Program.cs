1afa3b9c63ab1982677bb6e15730d452b302d66a
Danny Wilkison <danwilkison@gmail.com>
2019-11-13 21:22:55 -0500

    Add project files.

---
+++ CryptoChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
